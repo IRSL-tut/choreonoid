47cf951593e06c9bbec9349978d135c260d5397e
YoheiKakiuchi <kakiuchi.yohei.sw@tut.jp>
2024-06-05 19:19:20 +0900

    [WIP, VR] NOT working, local running openvr / use offscreen

--- ext/test_plugin/src/TestPlugin.cpp
+++ ext/test_plugin/src/TestPlugin.cpp
@@ -1,5 +1,3 @@
-#include "TestPlugin.h"
-
 #include <fmt/format.h>
 #define IRSL_DEBUG
 #include "irsl_debug.h"
@@ -7,9 +5,14 @@
 #include <cnoid/SceneWidget>
 #include <cnoid/SceneView>
 #include <cnoid/GLSLSceneRenderer>
-
+#include <cnoid/MessageView>
 #include <cnoid/App>
 #include <cnoid/Timer>
+
+#include "TestPlugin.h"
+#include "OffscreenGL.h"
+
+#include <openvr.h>
 
 using namespace cnoid;
 
@@ -27,37 +30,110 @@ public:
 
 public:
     Timer tm;
-    long counter;
+    unsigned long counter;
     double publishingRate;
+
+    OffscreenGL offGL;
+
+    vr::IVRSystem *m_pHMD;
+    GLSLSceneRenderer *slsr;
+    unsigned int nWidth, nHeight;
+    unsigned int uiResolveTextureId;
+    unsigned int uiResolveFramebufferId;
+
+    std::ostream *os_;
 };
 
 TestPlugin::Impl::Impl()
 {
+    m_pHMD = nullptr;
+    slsr = nullptr;
+    os_ = nullptr;
+    counter = 0;
 }
 void TestPlugin::Impl::initialize()
 {
-    SceneWidget *sw = SceneView::instance()->sceneWidget();
-    GLSceneRenderer *sr = sw->renderer<GLSceneRenderer>();
+    os_ = &(MessageView::instance()->cout(false));
 
-    GLSLSceneRenderer *slsr = static_cast<GLSLSceneRenderer *>(sr);
-    if (!!slsr) {
-        DEBUG_STREAM(" GLSL");
-        unsigned int uiResolveTextureId;
-        unsigned int uiResolveFramebufferId;
-        std::cerr << "aa" << std::endl;
-        bool res = slsr->makeBuffer(1200, 800, &uiResolveTextureId, &uiResolveFramebufferId);
-        DEBUG_STREAM(" GLSL: res: " << res);
+    //SceneWidget *sw = SceneView::instance()->sceneWidget();
+    //GLSceneRenderer *sr = sw->renderer<GLSceneRenderer>();
+    //slsr = static_cast<GLSLSceneRenderer *>(sr);
+
+    vr::EVRInitError eError = vr::VRInitError_None;
+    m_pHMD = vr::VR_Init( &eError, vr::VRApplication_Scene );
+    if ( eError != vr::VRInitError_None ) {
+        m_pHMD = NULL;
+        //printf( "Unable to init VR runtime: %s", vr::VR_GetVRInitErrorAsEnglishDescription( eError ) );
+        *os_ << "Unable to init VR runtime:  " << vr::VR_GetVRInitErrorAsEnglishDescription( eError ) << std::endl;
+        return;
     }
+    m_pHMD->GetRecommendedRenderTargetSize( &nWidth, &nHeight );
+    *os_ << "width x height = " << nWidth << " x  " << nHeight << std::endl;
 
+    vr::HmdMatrix44_t l_mat = m_pHMD->GetProjectionMatrix( vr::Eye_Left,  0.01f, 15.0f );
+    vr::HmdMatrix44_t r_mat = m_pHMD->GetProjectionMatrix( vr::Eye_Right, 0.01f, 15.0f );
+    vr::HmdMatrix34_t l_eye = m_pHMD->GetEyeToHeadTransform( vr::Eye_Left );
+    vr::HmdMatrix34_t r_eye = m_pHMD->GetEyeToHeadTransform( vr::Eye_Right );
+
+    {
+        bool glres = offGL.create();
+        *os_ << "offGL: create: " << glres << std::endl;
+        glres = offGL.makeCurrent();
+        *os_ << "offGL: current: " << glres << std::endl;
+        glres = offGL.makeBuffer(nWidth, nHeight, &uiResolveTextureId, &uiResolveFramebufferId);
+        *os_ << "offGL: buffer: " << glres << std::endl;
+
+        OffscreenGL::printSurfaceFormat(offGL.context->format(), *os_);
+    }
+    offGL.makeCurrent();
+    if ( !vr::VRCompositor() ) {
+        *os_ << "Compositor initialization failed. See log file for details" << std::endl;
+        return;
+    }
     tm.sigTimeout().connect( [this]() { this->singleLoop(); });
 
-    int interval_ms = 1000/33;
+    int interval_ms = 1000/5;
 
     tm.start(interval_ms);
 }
 void TestPlugin::Impl::singleLoop()
 {
     DEBUG_PRINT();
+    if (!!m_pHMD) {
+        //SceneWidget *sw = SceneView::instance()->sceneWidget();
+        //sw->makeCurrent();
+        std::vector<unsigned char> img(nWidth * nHeight * 3);
+        unsigned char *ptr = img.data();
+        unsigned char ret = counter++ % 0xFF;
+
+        for(int i = 0; i < nHeight; i++) {
+            for(int j = 0; j < nWidth; j++) {
+                ptr[3*(i*nWidth + j)] = 0xFF;
+                ptr[3*(i*nWidth + j)+1] = 0;
+                ptr[3*(i*nWidth + j)+2] = 0;
+            }
+        }
+        ////
+        *os_ << "up" << std::endl;
+        ////
+        offGL.makeCurrent();
+
+        vr::TrackedDevicePose_t m_rTrackedDevicePose[ vr::k_unMaxTrackedDeviceCount ];
+        vr::VRCompositor()->WaitGetPoses(m_rTrackedDevicePose, vr::k_unMaxTrackedDeviceCount, NULL, 0 );
+
+        offGL.writeTexture(uiResolveTextureId, ptr, nWidth, nHeight, 0, 0);
+
+        vr::Texture_t leftEyeTexture =  {(void*)(uintptr_t)uiResolveTextureId, vr::TextureType_OpenGL, vr::ColorSpace_Gamma };
+        vr::VRCompositor()->Submit(vr::Eye_Left,  &leftEyeTexture );
+
+        vr::Texture_t rightEyeTexture = {(void*)(uintptr_t)uiResolveTextureId, vr::TextureType_OpenGL, vr::ColorSpace_Gamma };
+        vr::VRCompositor()->Submit(vr::Eye_Right, &rightEyeTexture );
+
+        //// twice
+        vr::VRCompositor()->WaitGetPoses(m_rTrackedDevicePose, vr::k_unMaxTrackedDeviceCount, NULL, 0 );
+        //vr::Compositor_FrameTiming tmg;
+        //bool tm_q = vr::VRCompositor()->GetFrameTiming(&tmg);
+    }
 }
 
 TestPlugin* TestPlugin::instance()
