ede4cba3183ba9ef3c35cd18724ba1df0d18769c
Shin'ichiro Nakaoka <s.nakaoka@gmail.com>
2025-12-18 19:19:39 +0900

    Use std::optional instead of stdx::optional

--- src/AssimpSceneLoader/AssimpSceneLoader.cpp
+++ src/AssimpSceneLoader/AssimpSceneLoader.cpp
@@ -7,7 +7,7 @@
 #include <cnoid/UTF8>
 #include <cnoid/Format>
 #include <cnoid/stdx/filesystem>
-#include <cnoid/stdx/optional>
+#include <optional>
 #include <assimp/Importer.hpp>
 #include <assimp/postprocess.h>
 #include <assimp/scene.h>
@@ -50,7 +50,7 @@ public:
     filesystem::path directoryPath;
     ImageIO imageIO;
 
-    stdx::optional<Affine3f> T_local;
+    std::optional<Affine3f> T_local;
     
     typedef map<unsigned int, SgNodePtr> AiIndexToSgShapeMap;
     AiIndexToSgShapeMap aiIndexToSgShapeMap;
@@ -176,7 +176,7 @@ SgNode* AssimpSceneLoader::Impl::load(const std::string& filename)
     filesystem::path path(fromUTF8(filename));
     directoryPath = path.remove_filename();
 
-    T_local = stdx::nullopt;
+    T_local = std::nullopt;
 
     SgNode* node = convertAiNode(scene->mRootNode);
 
@@ -198,7 +198,7 @@ SgGroup* AssimpSceneLoader::Impl::convertAiNode(aiNode* node)
         S[1][0], S[1][1], S[1][2],
         S[2][0], S[2][1], S[2][2];
 
-    stdx::optional<Affine3f> prev_T_local = T_local;
+    std::optional<Affine3f> prev_T_local = T_local;
 
     if(ENABLE_FLIPPED_COORDINATE_EXPANSION || ENABLE_WARNING_FOR_FLIPPED_COORDINATE){
         double d = T.linear().determinant();
--- src/BodyPlugin/SimulatorItem.cpp
+++ src/BodyPlugin/SimulatorItem.cpp
@@ -320,7 +320,7 @@ public:
     double nextLogTime;
     double logTimeStep;
     
-    stdx::optional<int> extForceFunctionId;
+    std::optional<int> extForceFunctionId;
     std::mutex extForceMutex;
     struct ExtForceInfo {
         Link* link;
@@ -330,7 +330,7 @@ public:
     };
     ExtForceInfo extForceInfo;
 
-    stdx::optional<int> virtualElasticStringFunctionId;
+    std::optional<int> virtualElasticStringFunctionId;
     std::mutex virtualElasticStringMutex;
     struct VirtualElasticString {
         Link* link;
@@ -1813,8 +1813,8 @@ bool SimulatorItem::Impl::initializeSimulation(bool doReset)
         }
     }
 
-    extForceFunctionId = stdx::nullopt;
-    virtualElasticStringFunctionId = stdx::nullopt;
+    extForceFunctionId = std::nullopt;
+    virtualElasticStringFunctionId = std::nullopt;
 
     cloneMap.replacePendingObjects();
     
@@ -2669,7 +2669,7 @@ void SimulatorItem::clearExternalForces()
 {
     if(impl->extForceFunctionId){
         removePreDynamicsFunction(*impl->extForceFunctionId);
-        impl->extForceFunctionId = stdx::nullopt;
+        impl->extForceFunctionId = std::nullopt;
     }
 }    
 
@@ -2726,7 +2726,7 @@ void SimulatorItem::clearVirtualElasticStrings()
 {
     if(impl->virtualElasticStringFunctionId){
         removePreDynamicsFunction(*impl->virtualElasticStringFunctionId);
-        impl->virtualElasticStringFunctionId = stdx::nullopt;
+        impl->virtualElasticStringFunctionId = std::nullopt;
     }
 }
 
