150f6daf5027bb59bea84b7bf5eed39f91f76263
Shin'ichiro Nakaoka <s.nakaoka@gmail.com>
2026-08-08 13:08:07 +0900

    Fix the invalid member function pointer casts in the MessageOut bindings

--- src/Util/pybind11/PyMessageOut.cpp
+++ src/Util/pybind11/PyMessageOut.cpp
@@ -8,17 +8,31 @@ namespace cnoid {
 
 void exportPyMessageOut(py::module& m)
 {
-    py::class_<MessageOut, MessageOutPtr, Referenced>(m, "MessageOut")
+    py::class_<MessageOut, MessageOutPtr, Referenced> messageOut(m, "MessageOut");
+
+    py::enum_<MessageOut::MessageType>(messageOut, "MessageType")
+        .value("Normal", MessageOut::Normal)
+        .value("Highlighted", MessageOut::Highlighted)
+        .value("Warning", MessageOut::Warning)
+        .value("Error", MessageOut::Error)
+        .export_values();
+
+    messageOut
         .def_property_readonly_static("master", [](py::object){ return MessageOut::master(); })
         .def_property_readonly_static("interactive", [](py::object){ return MessageOut::interactive(); })
-        .def("put", (void(MessageOut::*)(const std::string&))&MessageOut::put)
-        .def("putln", (void(MessageOut::*)(const std::string&))&MessageOut::putln)
-        .def("putHighlighted", (void(MessageOut::*)(const std::string&))&MessageOut::putHighlighted)
-        .def("putHighlightedln", (void(MessageOut::*)(const std::string&))&MessageOut::putHighlightedln)
-        .def("putWarning", (void(MessageOut::*)(const std::string&))&MessageOut::putWarning)
-        .def("putWarningln", (void(MessageOut::*)(const std::string&))&MessageOut::putWarningln)
-        .def("putError", (void(MessageOut::*)(const std::string&))&MessageOut::putError)
-        .def("putErrorln", (void(MessageOut::*)(const std::string&))&MessageOut::putErrorln)
+        .def("put", (void(MessageOut::*)(const std::string&, int))&MessageOut::put,
+             py::arg("message"), py::arg("type") = (int)MessageOut::Normal)
+        .def("putln", (void(MessageOut::*)(const std::string&, int))&MessageOut::putln,
+             py::arg("message"), py::arg("type") = (int)MessageOut::Normal)
+        .def("notify", (void(MessageOut::*)(const std::string&, int))&MessageOut::notify,
+             py::arg("message"), py::arg("type") = (int)MessageOut::Normal)
+        .def("putHighlighted", &MessageOut::putHighlighted)
+        .def("putHighlightedln", &MessageOut::putHighlightedln)
+        .def("putWarning", &MessageOut::putWarning)
+        .def("putWarningln", &MessageOut::putWarningln)
+        .def("putError", &MessageOut::putError)
+        .def("putErrorln", &MessageOut::putErrorln)
+        .def("flush", &MessageOut::flush)
         ;
 }
 
--- src/Util/python/PyMessageOut.cpp
+++ src/Util/python/PyMessageOut.cpp
@@ -8,17 +8,31 @@ namespace cnoid {
 
 void exportPyMessageOut(nb::module_& m)
 {
-    nb::class_<MessageOut, Referenced>(m, "MessageOut")
+    nb::class_<MessageOut, Referenced> messageOut(m, "MessageOut");
+
+    nb::enum_<MessageOut::MessageType>(messageOut, "MessageType", nb::is_arithmetic())
+        .value("Normal", MessageOut::Normal)
+        .value("Highlighted", MessageOut::Highlighted)
+        .value("Warning", MessageOut::Warning)
+        .value("Error", MessageOut::Error)
+        .export_values();
+
+    messageOut
         .def_prop_ro_static("master", [](nb::handle){ return MessageOut::master(); })
         .def_prop_ro_static("interactive", [](nb::handle){ return MessageOut::interactive(); })
-        .def("put", (void(MessageOut::*)(const std::string&))&MessageOut::put)
-        .def("putln", (void(MessageOut::*)(const std::string&))&MessageOut::putln)
+        .def("put", (void(MessageOut::*)(const std::string&, int))&MessageOut::put,
+             nb::arg("message"), nb::arg("type") = (int)MessageOut::Normal)
+        .def("putln", (void(MessageOut::*)(const std::string&, int))&MessageOut::putln,
+             nb::arg("message"), nb::arg("type") = (int)MessageOut::Normal)
+        .def("notify", (void(MessageOut::*)(const std::string&, int))&MessageOut::notify,
+             nb::arg("message"), nb::arg("type") = (int)MessageOut::Normal)
         .def("putHighlighted", &MessageOut::putHighlighted)
         .def("putHighlightedln", &MessageOut::putHighlightedln)
         .def("putWarning", &MessageOut::putWarning)
         .def("putWarningln", &MessageOut::putWarningln)
         .def("putError", &MessageOut::putError)
         .def("putErrorln", &MessageOut::putErrorln)
+        .def("flush", &MessageOut::flush)
         ;
 }
 
